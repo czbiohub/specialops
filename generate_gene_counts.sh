d713fe9dcf4c41bdbe938644076e03208ecfa1d0
Amy Lyden <amy.m.lyden@gmail.com>
2018-06-08 15:23:34 -0700

    added comments to the script to better understand process

--- generate_gene_counts.sh
+++ generate_gene_counts.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-for i in `ls *.tab`; do cut -f2 $i > $i.o; done;
-paste `ls *.o` > all_counts.tsv
-cut -f1 `ls *.tab | head -1` > genenames.txt
-paste genenames.txt all_counts.tsv > gene_counts.tsv
-ls *.tab > filenames.txt
-tr "\n" "\t" < filenames.txt > filenames2.txt
+for i in `ls *.tab`; do cut -f2 $i > $i.o; done; #take the second column in tab file and make a new file with .o extension
+paste `ls *.o` > all_counts.tsv #paste together (column after column) all these new files with the second column only
+cut -f1 `ls *.tab | head -1` > genenames.txt #take the first column of one of the tab files and make a gene names file
+paste genenames.txt all_counts.tsv > gene_counts.tsv #make a new file with gene names, followed by the counts from each sample
+ls *.tab > filenames.txt #make a txt file with all the filenames
+tr "\n" "\t" < filenames.txt > filenames2.txt #pipe all the filenames.txt file into the tr command, which will replace enter with tab
 cat <(echo -ne '\t') filenames2.txt > filenames.txt # add tab to beginning of filenames file to columns line up
 #cat filenames.txt `echo "\n"` gene_counts.tsv > gene_counts_final.tsv
-awk '{print}' filenames.txt gene_counts.tsv > gene_counts_final.tsv
+awk '{print}' filenames.txt gene_counts.tsv > gene_counts_final.tsv #print out the filenames, followed by the gene counts in a row after row fashion
 
-
+#remove all unnecessary files
 rm *.o
 rm genenames.txt
 rm gene_counts.tsv 
 rm all_counts.tsv
 rm filenames2.txt
-rm filenames.txt
+
