59c5038e65ae46107e54f4b5b8e3d364a60874f8
Amy Lyden <amy.m.lyden@gmail.com>
2018-08-13 18:37:16 -0700

    added DASH pipeline and data formatting scripts

---
+++ DASH_Analysis/DASH_csv_format.py
@@ -0,0 +1,118 @@
+
+# coding: utf-8
+# In[1]: import pandas and read txt file from wrapper script into dataframe
+import pandas as pd
+df=pd.read_csv('DASHH_test.txt',delimiter='\s',encoding='utf-8', engine ='python', header=None)
+
+# In[3]:
+
+rows=list(range(len(df)))
+rows_to_drop=[2*x+1 for x in rows]
+rows_to_drop= [x for x in rows_to_drop if x<(len(df)+1)]
+
+
+# In[4]:
+
+
+df=df.drop(df.index[rows_to_drop])
+
+
+
+
+# In[5]:
+
+
+df = df[[3,5,7,9]]
+
+
+
+# In[6]:
+
+
+df=df.reset_index(drop=True)
+
+
+
+# In[7]:
+
+
+df=df.rename(index=int, columns={3 : "Guide Library", 5 : "Filename",7 : "Reads DASHed",9 : "Percent DASHed"})
+
+
+# In[8]:
+
+
+guidelibrary = df['Guide Library'].str.split('/',expand=True)
+
+
+
+# In[9]:
+
+
+df['Guide Library'] = guidelibrary[guidelibrary.columns[-1]]
+
+
+
+# In[10]:
+
+
+df['Percent DASHed']=df['Percent DASHed'].str.strip('%')
+
+
+
+
+# In[12]:
+
+
+reads = df['Reads DASHed'].str.split('/',expand=True)
+
+
+
+# In[14]:
+
+
+reads= reads.rename(index=int, columns={ 0 : "total reads DASHed", 1 : "total reads"})
+
+
+
+
+
+# In[16]:
+
+
+df=pd.concat([df, reads],axis=1)
+
+
+
+
+# In[18]:
+
+
+df=df.drop('Reads DASHed',axis=1)
+
+
+
+# In[19]:
+
+
+variables = df['Filename'].str.split('_',expand=True)
+
+
+
+# In[20]:
+
+variablestoadd = variables [[4,5,6,7,8,9,10,11]]
+variablestoadd=variablestoadd.rename(index=int, columns={ 4 : "DASH Experiment", 5 : "Subpool", 6 : "Sample ID", 7 : "Amount of Starting DNA", 8 : "Guides Used", 9 : "Cas9 Amount" , 10 : "Sequencing ID", 11: "Read 1 or 2" })
+
+
+# In[21]:
+
+
+df=pd.concat([df, variablestoadd],axis=1)
+
+
+
+# In[23]:
+
+
+df.to_csv("DASH_output.csv")
---
+++ DASH_Analysis/DASHwrapper.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# USAGE
+# bash DASHwrapper.sh awspath path_to_DASHguides outputfile
+
+aws_path=$1
+path_to_DASHguides=$2
+output_file=$3
+
+#sync and unzip files
+aws s3 sync $aws_path .
+rm Undetermined*
+gunzip *.gz
+
+#filter files using PriceSeqFilter
+for i in *R1_001.fastq ; do PriceSeqFilter -a 20 -fp ${i:0:-11}1_001.fastq ${i:0:-11}2_001.fastq -op filt-85-98-90_${i:0:-11}1.fq filt-85-98-90_${i:0:-11}2.fq -pair both -rqf 85 0.98 -rnf 90; done;
+
+#convert fastq to fasta
+for i in filt*; do seqtk seq -A $i > ${i:0:-1}asta; done;
+
+#cut TruSeq adaptors
+pip install cutadapt
+for i in filt*.fasta; do cutadapt -a GATCGGAAGAGCACACGTCT -o ${i:0:-6}_cutNEBF.fasta $i; done;
+for i in filt*_cutNEBF.fasta; do cutadapt -a AGATCGGAAGAGCACACGTCT -o ${i:0:-6}_cutNEBR.fasta $i; done;
+
+#run score_guides
+for i in filt*cutNEBR.fasta; do score_guides $path_to_DASHguides $i >> $output_file.txt; done
+
+tr "=" "," < $output_file.txt  > $output_file.csv
+printf "SampleFilename,PercentDASHable\n" > ${output_file}_unformatted.csv
+
+#remove intermediate files
+rm filt*cutNEBF.fasta
+rm filt*.fq
+rm filt*R1.fasta
+rm filt*R2.fasta
+
+#prepare to run python
+pip install pandas
+python DASH_csv_format_interactive.py $output_file
