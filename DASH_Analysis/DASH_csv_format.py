f59df0628b1c820e723439025f008a1f5f1cf3af
Amy Lyden <amy.m.lyden@gmail.com>
2018-09-19 11:38:55 -0700

    updating DASH scripts

--- DASH_Analysis/DASH_csv_format.py
+++ DASH_Analysis/DASH_csv_format.py
@@ -1,98 +1,47 @@
 
 # coding: utf-8
-# In[1]: import pandas and read txt file from wrapper script into dataframe
+
+# In[2]: import pandas
 import pandas as pd
-df=pd.read_csv('DASHH_test.txt',delimiter='\s',encoding='utf-8', engine ='python', header=None)
+import sys
+import os
 
-# In[3]:
+#take output file and read into dataframe
+txtfile=sys.argv[1]
+df=pd.read_csv("%s.txt" %txtfile,delimiter='\s',encoding='utf-8', engine ='python', header=None)
 
+
+# In[3]: create a list with rows to drop
 rows=list(range(len(df)))
 rows_to_drop=[2*x+1 for x in rows]
 rows_to_drop= [x for x in rows_to_drop if x<(len(df)+1)]
 
-
-# In[4]:
-
-
+# In[4]: drop all rows starting with total
 df=df.drop(df.index[rows_to_drop])
 
-
-
-
-# In[5]:
-
-
-df = df[[3,5,7,9]]
-
-
-
-# In[6]:
-
-
+# In[6]: reset the index to renumber everything
 df=df.reset_index(drop=True)
 
+# In[5]: select only the relevant columns from score_guides output
+df = df[[3,5,7,9]]
 
-
-# In[7]:
+# In[7]: rename columns
 
 
 df=df.rename(index=int, columns={3 : "Guide Library", 5 : "Filename",7 : "Reads DASHed",9 : "Percent DASHed"})
 
-
-# In[8]:
-
-
+# In[8]: parse out the name of the guide library file
 guidelibrary = df['Guide Library'].str.split('/',expand=True)
-
-
-
-# In[9]:
-
-
 df['Guide Library'] = guidelibrary[guidelibrary.columns[-1]]
 
-
-
-# In[10]:
-
-
+# In[12]: get rid of percent sign in percent DASHed
 df['Percent DASHed']=df['Percent DASHed'].str.strip('%')
 
-
-
-
-# In[12]:
-
-
+# In[13]: parse out the total reads vs DASHed reads
 reads = df['Reads DASHed'].str.split('/',expand=True)
-
-
-
-# In[14]:
-
-
 reads= reads.rename(index=int, columns={ 0 : "total reads DASHed", 1 : "total reads"})
-
-
-
-
-
-# In[16]:
-
-
 df=pd.concat([df, reads],axis=1)
-
-
-
-
-# In[18]:
-
-
 df=df.drop('Reads DASHed',axis=1)
 
-
-
-# In[23]:
-
-
-df.to_csv("DASH_output.csv")
+# In[23]: write to CSV file
+df.to_csv("%s.csv" %txtfile)
---
+++ DASH_Analysis/README.md
@@ -0,0 +1,26 @@
+# DASH analysis
+A wrapper script for subsampling, filtering, cutting TruSeq adaptors and running DASHit score guides on a directory of fastq files
+
+### Installation
+Transfer or copy the DASHwrapper.sh and the Python scripts (DASH_csv_format_interactive.py and DASH_csv_format.py) into the directory with the files you wish to perform the analysis on
+
+### Running the script
+
+Use bash to run the script and follow with 3 arguments
+1. the AWS path to your files
+2. the path to the DASH guide library you want use to score your files
+3. the name of your output file
+
+
+```
+bash DASHwrapper.sh s3://czbiohub-seqbot/fastqs/180907_A00111_0206_BH7W5WDSXX/rawdata/Amy_Lyden_AIH /mnt/data/nribo2_150_V2.csv AIH_Plate_02_1xPCR_DASHed
+```
+
+### Your output file
+
+Your CSV output file will contain five columns
+1. Guide library
+2. Your filename
+3. Total Reads DASHed (hit by scoreguides)
+4. Total Reads in sample
+5. Percent DASHed
