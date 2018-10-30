cfd538bea1c1283d8de3058d4a08c08511402e0f
Amy Lyden <amy.m.lyden@gmail.com>
2018-10-30 10:34:17 -0700

    adding iSeqPooling.sh

---
+++ Pooling_by_MiSeq/iSeqPooling.sh
@@ -0,0 +1,26 @@
+#$1 will be the file path to your aws bucket with the MiSeq reads (ex:s3://czbiohub-seqbot/fastqs/180612_M05295_0117_000000000-G1GKV/rawdata/)
+aws s3 sync $1 . --exclude "*" --include "*.fastq.gz"
+
+#search for the read counts
+zgrep -c "@FS" *R1*.gz > readcounts.txt
+
+#add the adaptor read counts
+zgrep -c ^GATCGGAAGAGCACACGTCT *R1*.gz > adaptorcounts.txt
+
+#cut the first column of adaptor counts
+cut -d: -f2 < adaptorcounts.txt > adaptorcountsnumbers.txt
+
+#convert : to ,
+tr ":" "," < readcounts.txt > readcounts.csv
+
+#add headers to your csv output, $2 will be the filename
+printf "SampleFilename,TotalReadCounts,AdaptorReadCounts\n" > $2.csv
+
+#add filenames/readcounts + adaptor read counts to csv output
+paste -d , readcounts.csv adaptorcountsnumbers.txt >> $2.csv
+
+#remove intermediate files
+rm readcounts.txt
+rm adaptorcounts.txt
+rm adaptorcountsnumbers.txt
+rm readcounts.csv
